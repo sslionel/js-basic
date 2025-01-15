881a2138988524671729daa50233de6572e8d582
Satwik Singh <92941886+sslionel@users.noreply.github.com>
2025-01-15 06:40:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //this text ensures that it will treat all js code as newer version of js.
+
+// alert(hello) this statement will create problem as we san't use it here it can be used only on console in browser.
+// there is a seperate syntax for alert in nodejs
+
+let name = "Satwik" //if we write in double quotes it is a string datatype
+let age = 3 // it is number datatype
+let isLoggedIn = true // it is boolean datatype
+
+
+// primitive datatypes:
+// number
+// bigint -> you can safely store and operate on large integers even beyond the safe integer limit 
+// string -> " "
+// boolean -> true ,false
+// null -> it is a standalone value which a representation of empty value
+// undefined -> when value is not assigned to a variable
+// symbol ->to show uniqueness of components symbol is used
+
+
+// non primitive datatypes:
+// object
+console.log(typeof null) // output is object which proves that null is an object
+console.log(typeof undefined) //output is undefined
+//array
+//regexp
